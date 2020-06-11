c0f631e7887e604caba4cd2a710d04a639dc489d
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-06-11 13:46:29 +0300

    minor fixes

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,34 +1,36 @@
 export default class Accordion {
-	constructor(trigger, message) {
-		this.trigger = document.querySelector(trigger),
-		this.message = document.querySelector(message)
+	constructor(triggers) {
+		this.triggers = document.querySelectorAll(triggers)
 	}
 
 	init() {
-		const durationTime = 500, // miliseconds
-				messageStyle = this.message.style,
-				marginMsg = window.getComputedStyle(this.message).marginTop;
+		this.triggers.forEach(trigger => {
+			const durationTime = 500, // miliseconds
+				message = trigger.closest(".module__info-show").nextElementSibling,
+				marginMsg = window.getComputedStyle(message).marginTop;
 
-		messageStyle.maxHeight = '0px';
-		messageStyle.marginTop = '0px';
-		messageStyle.transition = `all ${durationTime / 1000}s`;
-		messageStyle.overflow = 'hidden'
-		
-		this.trigger.addEventListener("click", () => {
-			if (window.getComputedStyle(this.message).display === "none") {
-				messageStyle.display = "block";
-				messageStyle.maxHeight = this.message.scrollHeight + "px";
-				messageStyle.marginTop = marginMsg;
-			} else {
-				messageStyle.maxHeight = messageStyle.marginTop = '0px';
-				setTimeout(() => {
-					messageStyle.display = "none";
-				}, durationTime)
-			}
-		});
+			message.style.maxHeight = '0px';
+			message.style.marginTop = '0px';
+			message.style.transition = `all ${durationTime / 1000}s`;
+			message.style.overflow = 'hidden'
+
+			trigger.addEventListener("click", () => {
+				if (window.getComputedStyle(message).display === "none") {
+					message.style.display = "block";
+					message.style.maxHeight = message.scrollHeight + "px";
+					message.style.marginTop = marginMsg;
+				} else {
+					message.style.maxHeight = message.style.marginTop = '0px';
+					setTimeout(() => {
+						message.style.display = "none";
+					}, durationTime)
+				}
+			});
+		})
+
 	}
 
 	// this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
 	// this.nextElementSibling.style.maxHeight = "0px";
-	
+
 }
